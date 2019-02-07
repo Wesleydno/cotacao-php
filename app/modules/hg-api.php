d088774606cb915e4a3c33344d0ec4ea78ba8b7e
Wesley Reis <wesleydno2011@gmail.com>
2019-02-07 03:31:36 -0200

    Criação da função genérica que trata os dados recebidos

--- app/modules/hg-api.php
+++ app/modules/hg-api.php
@@ -10,8 +10,35 @@ class HG_API
 	{
 		if (!empty($key))
 		 $this->key = $key;
+    }
+    
+
+    function request( $endpoint = "", $params = array() )
+	{
+	
+		$uri = "https://api.hgbrasil.com/". $endpoint ."?key=" . $this->key . "&format=json";
+
+		if ( is_array($params) ){
+
+			foreach ($params as $key => $value) {
+				if (empty($value)) continue;
+				$uri .= $key . '=' . urlencode($value) . '&';
+			}
+			$uri	  = substr($uri, 0, -1);
+			$response = @file_get_contents($uri);
+			$this->error = FALSE;
+			return json_decode($response, TRUE);
+
+		} 
+		else
+		{
+			$this->error = TRUE;
+			return FALSE;
+		}
+
 	}
 
 }
+
 
 ?>
